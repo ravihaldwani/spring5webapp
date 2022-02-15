bc4baa3b3d8abe2162c9a5ca7d829bd1d8edee9a
RaviHaldwani <ravihaldwani143@gmail.com>
2022-02-15 07:41:21 +0530

    spring initials

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5webappApplication {
+public class
+Spring5webappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
